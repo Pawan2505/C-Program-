2045786130b24c448e85f1f4784350371eea905c
pawan2505 <pawanaktu@gmail.com>
2024-07-08 11:56:25 +0530

    C Code

---
+++ C Program 11/array.c
@@ -0,0 +1,122 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[4] = {22, 33, 44, 55};
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d\n", arr[i]);
+//     }
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[4];
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("Please enter  %d index element:", i);
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Your Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[4];
+
+//     arr[0] = 23;
+//     arr[1] = 233;
+//     arr[2] = 243;
+//     arr[3] = 253;
+
+//     printf("Your Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// Updation / Modification
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[4];
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("Please enter  %d index element:", i);
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Your Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+
+//     // Modification
+
+//     arr[1] = 15;
+//     arr[3] = 99;
+
+//     printf("Your Updated  Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[4];
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("Please enter  %d index element:", i);
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Your Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+
+//     // Modification
+//     for (int i = 0; i < 4; i++)
+//     {
+//         if (i == 3)
+//         {
+//             arr[i] = 999;
+//         }
+//     }
+//     printf("\n");
+
+//     printf("Your Updated  Output is : ");
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+// }
